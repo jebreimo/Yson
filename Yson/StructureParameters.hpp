f96166755100408dbf84cd1948ca31a1a4b038e8
Jan Erik Breimo <jebreimo@gmail.com>
2018-07-27 20:20:00 +0200

    Fixing things g++ doesn’t like.

--- Yson/StructureParameters.hpp
+++ Yson/StructureParameters.hpp
@@ -6,6 +6,7 @@
 // License text is included with the source distribution.
 //****************************************************************************
 #pragma once
+#include <cstddef>
 #include "UBJsonValueType.hpp"
 
 namespace Yson
