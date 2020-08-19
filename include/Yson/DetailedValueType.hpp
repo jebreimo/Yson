ea6b39a8822f3d2a5df1255d835675cd837c2dfa
Jan Erik Breimo <jebreimo@gmail.com>
2020-08-20 00:01:51 +0200

    Remove one more -impl header. They make include analysis unnecessarily complicated.

--- include/Yson/DetailedValueType.hpp
+++ include/Yson/DetailedValueType.hpp
@@ -54,11 +54,46 @@ namespace Yson
     YSON_API bool isCompatible(DetailedValueType compatType,
                                DetailedValueType type);
 
+    namespace Details
+    {
+        template <typename T>
+        struct DetailedValueTraits
+        {
+            constexpr static DetailedValueType valueType()
+            {
+                return DetailedValueType::UNKNOWN;
+            }
+        };
+
+        #define YSON_DETAIL_DETAILEDVALUETRAITS(type, detValueType) \
+            template <> \
+            struct DetailedValueTraits<type> \
+            { \
+                constexpr static DetailedValueType valueType() \
+                {return detValueType;} \
+            }
+
+        YSON_DETAIL_DETAILEDVALUETRAITS(char, DetailedValueType::CHAR);
+        YSON_DETAIL_DETAILEDVALUETRAITS(int8_t, DetailedValueType::SINT_8);
+        YSON_DETAIL_DETAILEDVALUETRAITS(uint8_t, DetailedValueType::UINT_8);
+        YSON_DETAIL_DETAILEDVALUETRAITS(int16_t, DetailedValueType::SINT_16);
+        YSON_DETAIL_DETAILEDVALUETRAITS(uint16_t, DetailedValueType::UINT_16);
+        YSON_DETAIL_DETAILEDVALUETRAITS(int32_t, DetailedValueType::SINT_32);
+        YSON_DETAIL_DETAILEDVALUETRAITS(uint32_t, DetailedValueType::UINT_32);
+        YSON_DETAIL_DETAILEDVALUETRAITS(int64_t, DetailedValueType::SINT_64);
+        YSON_DETAIL_DETAILEDVALUETRAITS(uint64_t, DetailedValueType::UINT_64);
+        YSON_DETAIL_DETAILEDVALUETRAITS(float, DetailedValueType::FLOAT_32);
+        YSON_DETAIL_DETAILEDVALUETRAITS(double, DetailedValueType::FLOAT_64);
+        YSON_DETAIL_DETAILEDVALUETRAITS(std::string, DetailedValueType::STRING);
+    }
+
     /** @brief Returns true if a value of type T can hold a value of type
       *     @a valueType.
       */
     template <typename T>
-    bool isCompatible(DetailedValueType valueType);
+    bool isCompatible(DetailedValueType valueType)
+    {
+        auto type = Details::DetailedValueTraits<T>::valueType();
+        return isCompatible(type, valueType);
+    }
 }
-
-#include "DetailedValueType-impl.hpp"
