a52df67aedb597b6c351be370605e26b1fda73df
Jan Erik Breimo <jan_erikbreimo@trimble.com>
2022-06-07 10:58:54 +0200

    Add Yson.hpp, a convenient header that includes all of Yson.

---
+++ include/Yson/Yson.hpp
@@ -0,0 +1,15 @@
+//****************************************************************************
+// Copyright © 2022 Jan Erik Breimo. All rights reserved.
+// Created by Jan Erik Breimo on 07.06.2022.
+//
+// This file is distributed under the BSD License.
+// License text is included with the source distribution.
+//****************************************************************************
+#pragma once
+
+#include "JsonReader.hpp"
+#include "JsonReader.hpp"
+#include "ReaderIterators.hpp"
+#include "UBJsonReader.hpp"
+#include "UBJsonWriter.hpp"
+#include "YsonVersion.hpp"
